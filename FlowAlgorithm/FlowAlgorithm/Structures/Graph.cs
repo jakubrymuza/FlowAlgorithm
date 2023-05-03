f90ad36854634f93a83b5a056731b42fec3a1712
Jakub Rymuza <jakubrymuza@gmail.com>
2023-05-03 23:06:45 +0200

    add graph structure

--- FlowAlgorithm/FlowAlgorithm/Structures/Graph.cs
+++ FlowAlgorithm/FlowAlgorithm/Structures/Graph.cs
@@ -1,20 +1,55 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FlowAlgorithm.Structures
+﻿namespace FlowAlgorithm.Structures
 {
-    internal class Graph : IEnumerable
-    {        
-        public IEnumerator GetEnumerator() // po wierzchołkach ma być
+    internal class Graph
+    {
+        protected bool[,] _adjacencyMatrix;
+        protected int _size;
+
+        internal Graph(string path)
         {
-            for (int index = 0; index < 10; index++)
+            string fileText = File.ReadAllText(path);
+            string[] lines = fileText.Split('\n');
+
+            _size = int.Parse(lines[0]);
+
+            _adjacencyMatrix = new bool[_size, _size];
+
+            for (int i = 1; i < lines.Length; i++)
             {
-                yield return index;
+                var verts = lines[i].Split(" ");
+                _adjacencyMatrix[int.Parse(verts[0]) - 1, int.Parse(verts[1]) - 1] = true;
             }
         }
+
+        internal int GetSize() => _size;
+
+
+        internal bool HasEdge(int x, int y) => _adjacencyMatrix[x, y];
+
+        internal virtual IEnumerator<Edge> Edges()
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                for (int j = i; j < _size; j++)
+                    if (_adjacencyMatrix[i, j])
+                        yield return new Edge(i, j);
+            }
+        }
+
+        internal virtual IEnumerator<Edge> Neighbors(int x)
+        {
+            for (int i = 0; i < _size; i++)
+                if (_adjacencyMatrix[x, i])
+                    yield return new Edge(x, i);
+        }
+
+
+        protected Graph(int size)
+        {
+            _size = size;
+            _adjacencyMatrix = new bool[_size, _size];
+        }
+
+
     }
 }
