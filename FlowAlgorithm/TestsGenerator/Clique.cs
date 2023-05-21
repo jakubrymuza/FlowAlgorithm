59ed971c9a13122b2cb22561bd764e9080dc8aff
rafalsosnowski <rafalsosnowski4@wp.pl>
2023-05-21 21:19:38 +0200

    clique generator

---
+++ FlowAlgorithm/TestsGenerator/Clique.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerator
+{
+    internal class Clique
+    {
+        public static void GenerateClique(List<int> VerticlesCounts, string path)
+        {
+            for(int i = 0; i < VerticlesCounts.Count; i++) 
+            {
+                GenerateClique(VerticlesCounts[i], path, i);
+            }
+        }
+        private static void GenerateClique(int VerticlesCount, string path,int index)
+        {
+            using (StreamWriter writer =new($"{path}test{index}.txt"))
+            {
+                writer.WriteLine(VerticlesCount.ToString());
+
+                for(int i=1;i<= VerticlesCount;i++)
+                {
+                    for(int j=i+1;j<= VerticlesCount;j++)
+                    {
+                        writer.WriteLine($"{i} {j}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FlowAlgorithm/TestsGenerator/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TestsGenerator;
+
+Console.WriteLine("Hello, World!");
+
+
+Clique.GenerateClique(new() {1,2,3,4,5,6}, "C:\\Users\\rafci\\Desktop\\FlowAlgorithm\\FlowAlgorithm\\FlowAlgorithm\\Tests\\Cliques\\");
